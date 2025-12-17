ed8dab4cd5ae98e2fbb38bc74c87190a49a4cba6
Jota Uribe <uribe.dev@gmail.com>
2025-12-17 05:50:05 -0500

    Setting up deploy to GH pages

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/profile" element={<SetupProfile />} />
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react()],
+  base: '/social-network/',
   test: {
     globals: true,
     environment: 'jsdom',
